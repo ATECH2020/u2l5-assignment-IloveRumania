e097181612e6eb6bed3ae3aa1b72aa863e01e589
Lian James Modico <lianjam.157771@nv.ccsd.net>
2020-10-01 21:38:04 +0000

    Worked on Construction.

--- src/main/java/ConstructionTester.java
+++ src/main/java/ConstructionTester.java
@@ -6,12 +6,20 @@ public class ConstructionTester
     {
       double taxRate;//Sales tax.
       int windows;
-      int lumber;
-      double total;
-      //double grandTotal;
+      int boards;//AKA lumber
+      double total;//Subtotal
+      double grandTotal;
       Scanner in = new Scanner(System.in);
       System.out.println("Enter the sales tax rate: ");
       taxRate = in.nextDouble();
-      
+      System.out.println("How many boards do you need? ");
+      boards = in.nextInt();
+      System.out.println("How many windows do you need? ");
+      windows = in.nextInt();
+      Construction cost = new Construction(8, 11, taxRate);
+      total = cost.lumberCost(boards) + cost.windowCost(windows);
+      System.out.println("Total: " + total);
+      grandTotal = cost.grandTotal(total);
+      System.out.print("Grand Total: " + grandTotal);
     }
 }
